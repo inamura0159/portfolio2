2402d9fd22b1c1f81e3a46b51e42198d37148e25
inamura0159 <106084382+inamura0159@users.noreply.github.com>
2022-05-27 10:18:26 +0900

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,16 @@ $(function(){
         $('.bl_hamburger').toggleClass('is_close');
     });
 });
+
+//ビューワー
+$(function(){
+
+$('.bl_choice_list').click(function(){
+
+$('#header').attr('style', $(this).attr('style'));
+
+return false;
+//親要素へのイベント伝播（伝わり広まること）を止める役割
+});
+
+});
