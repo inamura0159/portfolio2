231ce468b8e1e60cddf9442642386662ec9fd007
inamura0159 <106084382+inamura0159@users.noreply.github.com>
2022-06-02 14:20:05 +0900

    Update script.js

--- script.js
+++ script.js
@@ -19,3 +19,32 @@ return false;
 });
 
 });
+
+
+//画像キャプション表示
+$(function(){
+$('.bl_cardUnit__extended').children('.bl_card').append('<div>');
+$('.bl_card').children('div').addClass('is_hover');
+
+$('.is_hover').each(function(){
+
+$(this).html(`<p>` + $(this).parent().children('.bl_card_imgWrapper').children('img').attr('alt')+ `</p>`);
+$(this).hide();
+});
+
+
+$('.bl_cardUnit__extended').children('.bl_card').hover(function(){
+$(this).children('div').stop().fadeIn(300);
+
+$(this).children('div').children('p').stop().animate({top: '50%'},300);
+
+},function(){
+
+
+$(this).children('div').stop().fadeOut(300);
+//キャプション部分のフェードアウト
+
+$(this).children('div').children('p').stop().animate({top: '60%'}, 300);
+
+});
+});
